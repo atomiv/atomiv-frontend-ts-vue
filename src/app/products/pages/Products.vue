9c5d6fa09f0de77db155ddce00186eae707b4e99
Igor Bogdanovic <igor.s.bogdanovic@gmail.com>
2020-09-13 13:58:15 +0200

    View - Products page #8

--- src/app/products/pages/Products.vue
+++ src/app/products/pages/Products.vue
@@ -1,25 +1,66 @@
 <template>
   <div>
     <h1>Products</h1>
+    <template v-if="products">
+      <Table
+        :value="productsList"
+        :columns="columns"
+        :sortData="sortData"
+        @sort="sortBy" />
+    </template>
   </div>
 </template>
 
 <script>
+import sortMethods from '@/-common-/mixins/sortMethods'
 import { createNamespacedHelpers } from 'vuex'
+
+import Table from '@/-common-/components/Table'
 
 const { mapState, mapActions } = createNamespacedHelpers('products')
 
 export default {
   name: 'Products',
 
-  created () {
-    this.loadProducts()
+  components: {
+    Table
+  },
+
+  mixins: [sortMethods],
+
+  data () {
+    return {
+      columns: [
+        { column: 'id', title: 'Id', sortable: true, size: 10 },
+        { column: 'code', title: 'Code', sortable: true },
+        { column: 'name', title: 'Product Name', sortable: true, size: 30 },
+        { column: 'price', title: 'Price', sortable: true},
+        { column: 'isListed', title: 'Listed', sortable: true},
+        { column: 'action', title: 'Action', rowValue: 'Details', linkTo: id => {
+          return { name: 'Products', params: { id } }
+        }}
+      ],
+
+      sortData: {
+        key: 'id',
+        order: 'asc'
+      }
+    }
   },
 
   computed: {
     ...mapState({
       products: state => state.products
-    })
+    }),
+
+    productsList () {
+      const list = this.products?.slice() || []
+      return this.sort(list)
+    }
+  },
+
+  created () {
+    this.loadProducts()
   },
 
   methods: {
@@ -32,9 +73,4 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/-common-/assets/style/settings/_module-settings';
-
-h1 {
-  @include fontSizeRem(20, 40);
-  text-align: center;
-}
 </style>
